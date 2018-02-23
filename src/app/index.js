f47329f8c41367bf360613aafc2f3a2a915cd56f
Tyler Bray <broncobray77@gmail.com>
2018-02-23 12:01:56 -0800

    first commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,15 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
 
-//Create a component
+var React = require('react');
+var ReactDom = require('react-dom');
+
+//create component
 var TodoComponent = React.createClass({
     render: function(){
         return(
-            <h1>Hellooooo</h1>
+            <h1>Hello!</h1>
         );
     }
 });
 
-ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
+//put component into html page
+ReactDom.render(<TodoComponent/>, document.getElementById(todo-wrapper))
